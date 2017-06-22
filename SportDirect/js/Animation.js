f279b747b38b1a6d7bd77575d8c226450d53ab38
mushmulcho <alex.goychev@gmail.com>
2017-06-22 15:33:59 +0300

    Update Animation.js

--- SportDirect/js/Animation.js
+++ SportDirect/js/Animation.js
@@ -4,7 +4,7 @@ var Animation = Animation || {};
 	var renderer;
 	var stage;
 	var resources =["present-box.png"];
-	var resourcesPath = "https://icons.iconseeker.com/png/fullsize/superbuuf/";//sportDirect/img/
+	var resourcesPath = "SportDirect/res/";
 	var oldPosY = 0;
 	var oldPosX = 0;
 
